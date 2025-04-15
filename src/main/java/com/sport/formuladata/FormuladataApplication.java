ed81885d698b412ce2fc2010f2588ffbc924c686
shapi88 <71607420+shapi88@users.noreply.github.com>
2025-04-15 22:50:24 +0100

    add - initial commit

---
+++ src/main/java/com/sport/formuladata/FormuladataApplication.java
@@ -0,0 +1,13 @@
+package com.sport.formuladata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormuladataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormuladataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sport/formuladata/FormuladataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sport.formuladata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormuladataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
