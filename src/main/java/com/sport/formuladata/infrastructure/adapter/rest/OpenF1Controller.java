767ba1ed0dff3ef3b1079dea3fb9ce09ab41ed89
shapi88 <71607420+shapi88@users.noreply.github.com>
2025-04-21 23:14:50 +0100

    Add - cors enablement to fe

--- src/main/java/com/sport/formuladata/infrastructure/adapter/rest/OpenF1Controller.java
+++ src/main/java/com/sport/formuladata/infrastructure/adapter/rest/OpenF1Controller.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/f1/data")
+@CrossOrigin(origins = "http://localhost:3000") // todo more generic
 public class OpenF1Controller {
     private final FetchOpenF1DataUseCase fetchOpenF1DataUseCase;
     private final GetMeetingsUseCase getMeetingsUseCase;
