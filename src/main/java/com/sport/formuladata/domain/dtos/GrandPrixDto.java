4d791c3d1f2dd07f9d2ac8c99a089a6f6edc5f55
shapi88 <71607420+shapi88@users.noreply.github.com>
2025-04-22 14:37:46 +0100

    Add - race enrich data with grandprix

---
+++ src/main/java/com/sport/formuladata/domain/dtos/GrandPrixDto.java
@@ -0,0 +1,11 @@
+package com.sport.formuladata.domain.dtos;
+
+public record GrandPrixDto(
+        String id,
+        String name,
+        String fullName,
+        String shortName,
+        String abbreviation,
+        //String countryId, // todo
+        Integer totalRacesHeld) {
+}
